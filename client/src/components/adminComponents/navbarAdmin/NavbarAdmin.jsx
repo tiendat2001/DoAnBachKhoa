edc5a13b908a8e577a93ad9d40d8752682ad6375
dat <damtiendat2001@gmail.com>
2024-04-02 18:10:33 +0700

    chinh css

--- client/src/components/adminComponents/navbarAdmin/NavbarAdmin.jsx
+++ client/src/components/adminComponents/navbarAdmin/NavbarAdmin.jsx
@@ -3,12 +3,15 @@ import "./navbarAdmin.css";
 // import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext } from "react";
 import React from "react"
+import { AuthContext } from "../../../context/AuthContext";
 
 const NavbarAdmin = () => {
 //   const { dispatch } = useContext(DarkModeContext);
+  const {user , dispatch} = useContext(AuthContext)
 
   return (
     <div className="navbarAdmin">
+        <h1 className="navbarAdmin_username">Hello, {user.username}</h1>
     </div>
   );
 };
