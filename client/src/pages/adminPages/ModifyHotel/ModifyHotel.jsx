dba738f564bf41a4ebcda43a4c2ae1f89fe10f4d
dat <damtiendat2001@gmail.com>
2024-04-05 19:32:22 +0700

    chinh modiFYHotel ( chua chinh dc anh)

--- client/src/pages/adminPages/ModifyHotel/ModifyHotel.jsx
+++ client/src/pages/adminPages/ModifyHotel/ModifyHotel.jsx
@@ -7,15 +7,66 @@ import axios from "axios";
 import { useEffect, useState, useContext } from "react";
 import useFetch from '../../../hooks/useFetch';
 import { AuthContext } from '../../../context/AuthContext';
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import { hotelInputs } from '../../../formSource';
+import { toast } from 'react-toastify';
+
 const ModifyHotel = () => {
     const location = useLocation();
-    const id = location.pathname.split("/")[3];
-    const { data, loading, error } = useFetch(`/hotels/find/${id}`);
+    const idHotel = location.pathname.split("/")[3];
+    const { data, loading, error } = useFetch(`/hotels/find/${idHotel}`);
     console.log(data)
     const [files, setFiles] = useState("");
     const [info, setInfo] = useState({});
     // const [rooms, setRooms] = useState([]);
     const { user } = useContext(AuthContext) // {user._id}
+
+    const handleChange = (e) => {
+        setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+      };
+
+      const handleClick = async (e) => {
+        e.preventDefault();
+        try {
+          const list = await Promise.all(
+            Object.values(files).map(async (file) => {
+              const data = new FormData();
+              data.append("file", file);
+              data.append("upload_preset", "upload");
+              const uploadRes = await axios.post(
+                "https://api.cloudinary.com/v1_1/tiendat2001/image/upload",
+                data
+              );
+              // console.log(uploadRes.data)
+              const { url } = uploadRes.data;
+              return url;
+            })
+          );
+    
+       
+            const newModifyHotel = {
+              ...info,
+            //   photos: list,
+              ownerId: user._id
+            };
+          
+    
+    
+            const Success = await axios.put(`/hotels/${idHotel}`, newModifyHotel);
+            if (Success) {
+              toast.success('Thành công chỉnh sửa!');
+            //   navigate("/admin/hotels");
+    
+            } else toast.error("Error.Please try again");
+    
+    
+         
+    
+    
+        } catch (err) {
+          console.log(error);
+        }
+      };
     return (
         <div className="listAdmin">
             <Sidebar />
@@ -48,7 +99,46 @@ const ModifyHotel = () => {
 
 
                         <div className="right">
-                            
+                            <div className="right">
+                                <form>
+                                    <div className="formInput">
+                                        <label htmlFor="file">
+                                            Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                                        </label>
+                                        <input
+                                            type="file"
+                                            id="file"
+                                            multiple
+                                            onChange={(e) => setFiles([...e.target.files])}
+                                            style={{ display: "none" }}
+                                        />
+                                    </div>
+
+                                    {/* render các trường */}
+                                    {hotelInputs.map((input) => (
+                                        <div className="formInput" key={input.id}>
+                                            <label>{input.label}</label>
+                                            <input
+                                                id={input.id}
+                                                onChange={handleChange}
+                                                type={input.type}
+                                                value={data[input.id] }
+                                            />
+                                        </div>
+                                    ))}
+                                    {/* test */}
+                                    <label>Hotel description</label>
+                                    <textarea
+                                        id="desc"
+                                        rows="4" /* Số dòng mặc định hiển thị ban đầu */
+                                        onChange={handleChange}
+                                        style={{ width: "100%", padding: "10px", fontSize: "16px", border: "1px solid #ccc", borderRadius: "5px", boxSizing: "border-box" }}
+                                        value={data.desc }
+                                    ></textarea>
+                                    <button onClick={handleClick}>Send</button>
+                                </form>
+                            </div>
+
                         </div>
                     </div>
                 </div>
