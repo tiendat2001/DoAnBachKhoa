4221dcb138e3340fa68311165a0e5621a9b13728
dat <damtiendat2001@gmail.com>
2024-04-07 17:37:50 +0700

    update listRoom

---
+++ client/src/datatablesource.js
@@ -0,0 +1,28 @@
+export const roomColumns = [
+    { field: "_id", headerName: "ID", width: 230 },
+    {
+      field: "title",
+      headerName: "Title",
+      width: 150,
+    },
+    {
+      field: "desc",
+      headerName: "Description",
+      width: 200,
+    },
+    {
+      field: "hotelName",
+      headerName: "Hotel",
+      width: 200,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 130,
+    },
+    {
+      field: "maxPeople",
+      headerName: "Max People",
+      width: 130,
+    },
+  ];
--- client/src/pages/adminPages/ListRoom/ListRoom.jsx
+++ client/src/pages/adminPages/ListRoom/ListRoom.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Sidebar from '../../../components/adminComponents/sidebar/Sidebar'
 import NavbarAdmin from '../../../components/adminComponents/navbarAdmin/NavbarAdmin'
+import "./listRoom.css"
+import { DataGrid } from "@mui/x-data-grid";
+import { roomColumns } from '../../../datatablesource';
 const ListRoom = () => {
     return (
         <div className="listAdmin">
