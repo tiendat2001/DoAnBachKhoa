275c4c5950341e3806a620a4cd7425f233aa4fa4
dat <damtiendat2001@gmail.com>
2024-04-09 11:54:25 +0700

    Update ListRoomClient.jsx

--- client/src/components/ListRoomClient/ListRoomClient.jsx
+++ client/src/components/ListRoomClient/ListRoomClient.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import axios from "axios";
 import { AuthContext } from '../../context/AuthContext';
 import useFetch from '../../hooks/useFetch';
+import "./listRoomClient.css"
 const ListRoomClient = ({hotelId}) => {
 
 const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
     console.log(data)
   return (
-    <div>
-      <h1>ddd</h1>
+    <div className="RoomClientContainer">
+      {data.map((item) => (
+        <div className="flex_div">
+          <div>
+            <h1>{item.title}</h1>
+            <h2>{item.maxPeople}</h2>
+            <h2>{item.desc}</h2>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
