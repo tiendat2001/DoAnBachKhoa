f7fb8fab273421821038dbcd5f155391443c5b2a
dat <damtiendat2001@gmail.com>
2024-04-09 12:37:34 +0700

    updảte giao dien listRoom client

--- client/src/components/ListRoomClient/ListRoomClient.jsx
+++ client/src/components/ListRoomClient/ListRoomClient.jsx
@@ -3,19 +3,40 @@ import axios from "axios";
 import { AuthContext } from '../../context/AuthContext';
 import useFetch from '../../hooks/useFetch';
 import "./listRoomClient.css"
-const ListRoomClient = ({hotelId}) => {
+const ListRoomClient = ({ hotelId }) => {
 
-const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
-    console.log(data)
+  const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
+  console.log(data)
   return (
     <div className="RoomClientContainer">
       {data.map((item) => (
-        <div className="flex_div">
-          <div>
-            <h1>{item.title}</h1>
-            <h2>{item.maxPeople}</h2>
-            <h2>{item.desc}</h2>
+        <div className="flex_div" style={{ border: '1px solid #7fc7af', padding: '10px' }}>
+
+          <div style={{width:'50%'}} >
+            <div  className="rTitle">{item.title}</div>
+            <div className="rDesc">Số lượng người: {item.maxPeople}</div>
+            <div className="rMax">{item.desc}</div>
           </div>
+
+          <div>
+            Giá phòng: {item.price}
+          </div>
+
+          <div style={{ marginBottom: '10px' }}>
+            <select>
+              <option value="1">1 phòng</option>
+              <option value="2">2 phòng</option>
+              <option value="3">3 phòng</option>
+              {/*Thêm các tùy chọn khác nếu cần*/}
+            </select>
+          </div>
+
+          <div>
+            <input type="checkbox" id="checkbox" name="checkbox" value="checkbox" />
+            <label htmlFor="checkbox"></label>
+          </div>
+
+
         </div>
       ))}
     </div>
