fa8c63deeb84743f0d1ec1863c9b0d2db54e4861
dat <damtiendat2001@gmail.com>
2024-04-09 23:50:56 +0700

    update listRoomCLient

--- client/src/components/ListRoomClient/ListRoomClient.jsx
+++ client/src/components/ListRoomClient/ListRoomClient.jsx
@@ -7,15 +7,16 @@ const ListRoomClient = ({ hotelId }) => {
 
   const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
 
-  
+
 
   return (
     <div className="RoomClientContainer">
+      <h1>Danh sách phòng</h1>
       {data.map((item) => (
         <div className="flex_div" style={{ border: '1px solid #7fc7af', padding: '10px' }}>
 
-          <div style={{width:'50%'}} >
-            <div  className="rTitle">{item.title}</div>
+          <div style={{ width: '50%' }} >
+            <div className="rTitle">{item.title}</div>
             <div className="rDesc">Số lượng người: {item.maxPeople}</div>
             <div className="rMax">{item.desc}</div>
           </div>
@@ -24,18 +25,28 @@ const ListRoomClient = ({ hotelId }) => {
             Giá phòng: {item.price}
           </div>
 
-          <div style={{ marginBottom: '10px' }}>
+          {/* <div style={{ marginBottom: '10px' }}>
             <select>
               <option value="1">1 phòng</option>
               <option value="2">2 phòng</option>
               <option value="3">3 phòng</option>
-              {/*Thêm các tùy chọn khác nếu cần*/}
             </select>
-          </div>
+          </div> */}
 
-          <div>
-            <input type="checkbox" id="checkbox" name="checkbox" value="checkbox" />
-            <label htmlFor="checkbox"></label>
+          <div className="rSelectRooms" style={{ width: '30%' }}>
+            {item.roomNumbers.map((roomNumber) => (
+              <div className="room">
+                {/* <label>{roomNumber.number}</label> */}
+                {/*  lam the nao truyen them item.price */}
+                <input
+                  type="checkbox"
+                  value={roomNumber._id}
+                  // onClick={(e) => handleSelect(e, item.price, roomNumber.number)}
+                  // disabled={!isAvailable(roomNumber)}
+                />
+              </div>
+            ))}
+            {/* <label htmlFor="checkbox"></label> */}
           </div>
 
 
