2210fb168a0b30ba2a395c58e429c6c4ba447884
dat <damtiendat2001@gmail.com>
2024-04-10 12:14:45 +0700

    Update ListRoomClient.jsx

--- client/src/components/ListRoomClient/ListRoomClient.jsx
+++ client/src/components/ListRoomClient/ListRoomClient.jsx
@@ -10,20 +10,30 @@ const ListRoomClient = ({ hotelId }) => {
   const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
   const [selectedRooms, setSelectedRooms] = useState([]);
 
-  const handleSelect = (e) => {
+  // lấy roomNumber_id theo tích
+  const handleSelectCheckBox = (e) => {
     const checked = e.target.checked;
     const value = e.target.value;
     setSelectedRooms(
       checked
         ? [...selectedRooms, value]
-        : selectedRooms.filter((item) => item !== value)
+        : selectedRooms.filter((item) => item !== value) // nếu bỏ tích thì bỏ phòng có value đấy ra mảng, giữ lại phòng kp value
     );
   };
-  console.log(selectedRooms)
+  const isAvailable = (roomNumber) => {
+    // const isFound = roomNumber.unavailableDates.some((date) =>
+    //   alldates.includes(new Date(date).getTime())
+    // );
+
+    // return !isFound;
+    return false
+  };
+
+  // console.log(selectedRooms)
 
   return (
     <div className="RoomClientContainer">
-      <h1>Danh sách phòng</h1>
+      <h1>Bạn muốn đặt phòng?</h1>
       {data.map((item) => (
         <div className="flex_div" style={{ border: '1px solid #7fc7af', }}>
 
@@ -35,6 +45,8 @@ const ListRoomClient = ({ hotelId }) => {
 
           <div>
             Giá phòng: {item.price}
+            <br />
+            (Mỗi đêm)
           </div>
 
           {/* <div style={{ marginBottom: '10px' }}>
@@ -46,17 +58,17 @@ const ListRoomClient = ({ hotelId }) => {
           </div> */}
 
           <div className="rSelectRooms" style={{ width: '20%' }}>
-            <div style={{ display: 'flex', flexWrap: 'wrap' , width: '70%', alignItems: 'center'  }}>
+            <div style={{ display: 'flex', flexWrap: 'wrap', width: '70%', alignItems: 'center' }}>
               {item.roomNumbers.map((roomNumber) => (
-                <div className="room">
-                  <input
-                    type="checkbox"
-                    value={roomNumber._id}
-                    onChange={handleSelect}
-                  // onClick={(e) => handleSelect(e, item.price, roomNumber.number)}
-                  // disabled={!isAvailable(roomNumber)}
-                  />
-                </div>
+                isAvailable(roomNumber) ? (
+                  <div className="room" key={roomNumber._id}>
+                    <input
+                      type="checkbox"
+                      value={roomNumber._id}
+                      onChange={handleSelectCheckBox}
+                    />
+                  </div>
+                ) : null
               ))}
             </div>
 
