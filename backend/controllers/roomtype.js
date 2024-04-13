e1a575fa82f159434f7f654483259c5cac9b46a6
dat <damtiendat2001@gmail.com>
2024-04-13 14:26:25 +0700

    doi lai thanh pull

--- backend/controllers/roomtype.js
+++ backend/controllers/roomtype.js
@@ -88,7 +88,7 @@ export const deleteRoom = async (req, res, next) => {
     await Room.findByIdAndDelete(req.params.id);
     try {
       await Hotel.findByIdAndUpdate(deleteRoom.hotelId, {
-        $push: { rooms: req.params.id },
+        $pull: { rooms: req.params.id },
       });
     } catch (err) {
       next(err);
