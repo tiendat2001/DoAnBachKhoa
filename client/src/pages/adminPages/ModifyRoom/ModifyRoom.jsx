13c295eebc1dc6270f75df1e16bab2f0bab792b5
dat <damtiendat2001@gmail.com>
2024-04-15 12:30:59 +0700

    Update ModifyRoom.jsx

--- client/src/pages/adminPages/ModifyRoom/ModifyRoom.jsx
+++ client/src/pages/adminPages/ModifyRoom/ModifyRoom.jsx
@@ -3,12 +3,14 @@ import Sidebar from '../../../components/adminComponents/sidebar/Sidebar'
 import NavbarAdmin from '../../../components/adminComponents/navbarAdmin/NavbarAdmin'
 import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext ,} from "react";
 import useFetch from '../../../hooks/useFetch';
 import { AuthContext } from '../../../context/AuthContext';
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { roomInputs } from '../../../formSource';
 import { toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom";
+
 const ModifyRoom = () => {
     const location = useLocation();
     const idRoom = location.pathname.split("/")[3];
@@ -17,6 +19,58 @@ const ModifyRoom = () => {
     const [files, setFiles] = useState("");
     const [info, setInfo] = useState({});
     const { user } = useContext(AuthContext) // {user._id}
+    const [isSending, setIsSending] = useState(false);
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+      };
+
+      // khi người dùng submit
+      const handleClick = async (e) => {
+        e.preventDefault();
+        try {
+          const list = await Promise.all(
+            Object.values(files).map(async (file) => {
+              const data = new FormData();
+              data.append("file", file);
+              data.append("upload_preset", "upload");
+              const uploadRes = await axios.post(
+                "https://api.cloudinary.com/v1_1/tiendat2001/image/upload",
+                data
+              );
+              // console.log(uploadRes.data)
+              const { url } = uploadRes.data;
+              return url;
+            })
+          );
+    
+       
+            const newModifyRoom = {
+              ...info,
+              ...(list.length > 0 && { photos: list }), // nếu người dùng có thêm ảnh vào thì set lại ảnh mới, ko thì giữ nguyên
+              ownerId: user._id
+            };
+          
+            
+            // console.log(list.length)
+    
+            const Success = await axios.put(`/rooms/${idRoom}`, newModifyRoom);
+            if (Success) {
+              toast.success('Thành công chỉnh sửa!');
+            //   navigate("/admin/rooms");
+    
+            } else toast.error("Error.Please try again");
+    
+    
+         
+    
+    
+        } catch (err) {
+          console.log(error);
+        }
+      };
+
     return (
         <div className="listAdmin">
             <Sidebar />
@@ -25,6 +79,7 @@ const ModifyRoom = () => {
                 {/* css từ newHotel.css */}
                 <div className="top">
                     <h1>Chỉnh sửa loại phòng</h1>
+                    {/* <h1>Điền vào trường cần sửa thông tin</h1> */}
                 </div>
 
                 <div className="bottom">
@@ -72,9 +127,9 @@ const ModifyRoom = () => {
                                     <label>{input.label}</label>
                                     <input
                                         id={input.id}
-                                        // onChange={handleChange}
+                                        onChange={handleChange}
                                         type={input.type}
-                                        placeholder={input.placeholder}
+                                        placeholder={data[input.id]}
                                     />
                                 </div>
                             ))}
@@ -83,13 +138,14 @@ const ModifyRoom = () => {
                             <textarea
                                 id="desc"
                                 rows="4" /* Số dòng mặc định hiển thị ban đầu */
-                                // onChange={handleChange}
+                                onChange={handleChange}
+                                placeholder={data.desc }
                                 style={{ width: "100%", padding: "10px", fontSize: "16px", border: "1px solid #ccc", borderRadius: "5px", boxSizing: "border-box" }}
                             ></textarea>
-
-                            {/* <button onClick={handleClick} disabled={isSending}>
-                                {isSending ? 'Sending...' : 'Thêm loại phòng'}
-                            </button>             */}
+                            
+                            <button onClick={handleClick} disabled={isSending}>
+                                {isSending ? 'Sending...' : 'Lưu thông tin loại phòng'}
+                            </button>            
                             </form>
                     </div>
 
