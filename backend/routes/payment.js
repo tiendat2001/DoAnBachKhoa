8006ad15b49b2df4ef27b265f6f092d0228a417c
dat <damtiendat2001@gmail.com>
2024-07-14 17:00:26 +0700

    chưa xog

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -10,58 +10,60 @@ import Room from "../models/RoomType.js";
 import Hotel from "../models/Hotel.js";
 import axios from 'axios';
 import request from "request"
-router.post("/create_payment_url", function (req, res, next) {
-  process.env.TZ = "Asia/Ho_Chi_Minh";
-
+import dateFormat from "dateformat"
+router.post('/create_payment_url', function (req, res, next) {
+    
+  process.env.TZ = 'Asia/Ho_Chi_Minh';
+  
   let date = new Date();
-  let createDate = moment(date).format("YYYYMMDDHHmmss");
+  let createDate = moment(date).format('YYYYMMDDHHmmss');
+  let expireDate = moment(date).add(7, 'days').format('YYYYMMDDHHmmss'); 
+  let ipAddr = req.headers['x-forwarded-for'] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
 
-  let ipAddr =
-    req.headers["x-forwarded-for"] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
-
-
-  let tmnCode = config.get("vnp_TmnCode");
-  let secretKey = config.get("vnp_HashSecret");
-  let vnpUrl = config.get("vnp_Url");
-  let returnUrl = config.get("vnp_ReturnUrl");
-  // bth 3 tham so nay lay tu req.body
-  let orderId = req.body.reservationId;
+  
+  let tmnCode = config.get('vnp_TmnCode');
+  let secretKey = config.get('vnp_HashSecret');
+  let vnpUrl = config.get('vnp_Url');
+  let returnUrl = config.get('vnp_ReturnUrl');
+  let orderId = moment(date).format('DDHHmmss');
   let amount = req.body.amount;
-  let bankCode = req.body.paymentType; //INTCARD
-
-  let locale = "vn";
-  if (locale === null || locale === "") {
-    locale = "vn";
+  let bankCode = req.body.bankCode;
+  
+  let locale = req.body.language;
+  if(locale === null || locale === ''){
+      locale = 'vn';
   }
-  let currCode = "VND";
+  let currCode = 'VND';
   let vnp_Params = {};
-  vnp_Params["vnp_Version"] = "2.1.0";
-  vnp_Params["vnp_Command"] = "pay";
-  vnp_Params["vnp_TmnCode"] = tmnCode;
-  vnp_Params["vnp_Locale"] = locale;
-  vnp_Params["vnp_CurrCode"] = currCode;
-  vnp_Params["vnp_TxnRef"] = orderId;
-  vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
-  vnp_Params["vnp_OrderType"] = "other";
-  vnp_Params["vnp_Amount"] = amount * 100;
-  vnp_Params["vnp_ReturnUrl"] = returnUrl;
-  vnp_Params["vnp_IpAddr"] = ipAddr;
-  vnp_Params["vnp_CreateDate"] = createDate;
-  if (bankCode !== null && bankCode !== "") {
-    vnp_Params["vnp_BankCode"] = bankCode;
-  }
+  vnp_Params['vnp_Version'] = '2.1.0';
+  vnp_Params['vnp_Command'] = 'pay';
+  vnp_Params['vnp_TmnCode'] = tmnCode;
+  vnp_Params['vnp_Locale'] = 'vn';
+  vnp_Params['vnp_CurrCode'] = currCode;
+  vnp_Params['vnp_TxnRef'] = orderId;
+  vnp_Params['vnp_OrderInfo'] = 'Thanh toan cho ma GD:' + orderId;
+  vnp_Params['vnp_OrderType'] = 'other';
+  vnp_Params['vnp_Amount'] = amount * 100;
+  vnp_Params['vnp_ReturnUrl'] = returnUrl;
+  vnp_Params['vnp_IpAddr'] = ipAddr;
+  vnp_Params['vnp_CreateDate'] = createDate;
+  vnp_Params['vnp_ExpireDate'] = expireDate;
+
+  // if(bankCode !== null && bankCode !== ''){
+  //     vnp_Params['vnp_BankCode'] = bankCode;
+  // }
 
   vnp_Params = sortObject(vnp_Params);
 
   let signData = querystring.stringify(vnp_Params, { encode: false });
   let hmac = crypto.createHmac("sha512", secretKey);
-  let signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
-  vnp_Params["vnp_SecureHash"] = signed;
-  vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
-  // TRẢ VỀ LÀ 1 HTML
+  let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+  vnp_Params['vnp_SecureHash'] = signed;
+  vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
   res.set("Content-Type", "text/html");
   res.send(JSON.stringify(vnpUrl));
 });
@@ -141,9 +143,9 @@ router.get("/vnpay_ipn", async function (req, res, next) {
             //paymentStatus = '1'
             // Ở đây cập nhật trạng thái giao dịch thanh toán thành công vào CSDL của bạn
             const updatedReservation = await Reservation.findByIdAndUpdate(
-              orderId, 
+              orderId,
               { status: 1 },
-              { new: true } 
+              { new: true }
             );
 
             // send email
@@ -173,27 +175,27 @@ router.get("/vnpay_ipn", async function (req, res, next) {
             //paymentStatus = '2'
             // Ở đây cập nhật trạng thái giao dịch thanh toán thất bại vào CSDL của bạn
             try {
-               const reservationToDelete = await Reservation.findById(orderId);
-               for (const roomTypeId of reservationToDelete.roomTypeIdsReserved) {
+              const reservationToDelete = await Reservation.findById(orderId);
+              for (const roomTypeId of reservationToDelete.roomTypeIdsReserved) {
                 for (var i = 0; i < roomTypeId.quantity; i++) {
                   try {
                     const res = await axios.put(`http://localhost:8800/api/rooms/cancelAvailability/${roomTypeId.roomTypeId}`, {
                       dates: reservationToDelete.allDatesReserve,
-                      unavailableRangeDates:{
-                        startDateRange:reservationToDelete.start,
-                        endDateRange:reservationToDelete.end
+                      unavailableRangeDates: {
+                        startDateRange: reservationToDelete.start,
+                        endDateRange: reservationToDelete.end
                       }
                     });
                     // console.log(`Room ${roomId} updated successfully.`);
                   } catch (err) {
                     console.log(err);
-                  
+
                   }
                 }
               }
 
 
-             // xóa reservation
+              // xóa reservation
               const reservation = await Reservation.findByIdAndDelete(orderId);
 
 
@@ -311,9 +313,9 @@ router.post("/refund", function (req, res, next) {
   let vnp_Api = config.get("vnp_Api");
 
   let vnp_TxnRef = req.body.orderId;
-  let vnp_TransactionDate =  moment(date).format("YYYYMMDDHHmmss");
+  let vnp_TransactionDate = moment(date).format("YYYYMMDDHHmmss");
   let vnp_Amount = req.body.amount * 100;
-  
+
   let vnp_TransactionType = "03"
 
   let vnp_CreateBy = req.body.user;
@@ -394,19 +396,19 @@ router.post("/refund", function (req, res, next) {
 });
 
 function sortObject(obj) {
-  let sorted = {};
-  let str = [];
-  let key;
-  for (key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      str.push(encodeURIComponent(key));
+	let sorted = {};
+	let str = [];
+	let key;
+	for (key in obj){
+		if (obj.hasOwnProperty(key)) {
+		str.push(encodeURIComponent(key));
+		}
+	}
+	str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
     }
-  }
-  str.sort();
-  for (key = 0; key < str.length; key++) {
-    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
-  }
-  return sorted;
+    return sorted;
 }
 
 export default router
