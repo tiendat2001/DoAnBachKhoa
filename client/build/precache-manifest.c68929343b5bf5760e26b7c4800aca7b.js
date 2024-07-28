739845aac31841e4ee0aba1d52938e624a0ce49f
tiendat2001 <75568539+tiendat2001@users.noreply.github.com>
2024-07-28 13:40:33 +0700

    Add files via upload

---
+++ client/build/precache-manifest.c68929343b5bf5760e26b7c4800aca7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edaa3502a3e932d5b6c4f660d6ad79b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "275b91371f63491fd0a9",
+    "url": "/static/css/2.1b5c8740.chunk.css"
+  },
+  {
+    "revision": "e2ded956c499c3a3311a",
+    "url": "/static/css/main.2e108954.chunk.css"
+  },
+  {
+    "revision": "275b91371f63491fd0a9",
+    "url": "/static/js/2.886857a1.chunk.js"
+  },
+  {
+    "revision": "e2ded956c499c3a3311a",
+    "url": "/static/js/main.633db66e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
